98b1f7b1461e758cb5488c9ce988ea8368a65166
caaraya <christina-araya@hotmail.com>
2018-06-07 22:04:15 -0700

    Christina - added functional allarm (with receiver) to watch, standalone, with no input data(basic) cannot automatically repeat allarm

---
+++ Mobile/DrWatch-mobile/DrWatch-wearos/DrWatch-wearos/AlarmReceiver.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace DrWatch_wearos
+{
+    [BroadcastReceiver]
+    class AlarmReceiver : BroadcastReceiver
+    {
+
+        public override void OnReceive(Context context, Intent intent)
+        {
+            // set next alarm
+            var repetitionIntent = new Intent(context, typeof(AlarmReceiver));
+            var source = PendingIntent.GetBroadcast(context, 0, intent, 0);
+            var am = (AlarmManager)Android.App.Application.Context.GetSystemService(Context.AlarmService);
+            var calendar = DateTime.Parse(intent.GetStringExtra(Intent.ExtraText));
+
+            DateTime dtBasis = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+            am.SetExactAndAllowWhileIdle(AlarmType.RtcWakeup, (long)calendar.ToUniversalTime().Subtract(dtBasis).TotalMilliseconds, source);
+
+        }
+    }
+}
--- Mobile/DrWatch-mobile/DrWatch-wearos/DrWatch-wearos/MainActivity.cs
+++ Mobile/DrWatch-mobile/DrWatch-wearos/DrWatch-wearos/MainActivity.cs
@@ -11,10 +11,12 @@ using Android.Support.V4.App;
 using Android.Support.V4.View;
 using Android.Support.Wearable.Activity;
 using Java.Interop;
-using Android.Views.Animations;
+using System.Globalization;
+using Android.Graphics;
 
 namespace DrWatch_wearos
 {
+
     [Activity(Label = "@string/app_name", MainLauncher = true)]
     public class MainActivity : WearableActivity
     {
@@ -27,7 +29,33 @@ namespace DrWatch_wearos
 
             _textView = FindViewById<TextView>(Resource.Id.text);
             SetAmbientEnabled();
+
+            DateTime nowtime = DateTime.Now.AddMinutes(2.0);
+
+            setAlarm(nowtime);
         }
+
+        //set our alarm with a datetime object
+        private void setAlarm(DateTime calendar) {
+            Color c = Color.Red;
+            _textView.SetTextColor(c);
+            
+            // set my alarm (repeating function taken out)
+            Intent intent = new Intent(this, typeof(AlarmReceiver));
+           
+            // add information to pending intent !!!
+            DateTime tmp = calendar;
+            tmp.AddMinutes(2.0);
+            intent.PutExtra(Intent.ExtraText, tmp.ToLongTimeString());
+            PendingIntent pendingIntent = PendingIntent.GetBroadcast(this, 14532, intent, 0);
+            AlarmManager alarmManager = (AlarmManager)GetSystemService(Context.AlarmService);
+            
+            DateTime dtBasis = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+            alarmManager.SetExactAndAllowWhileIdle(AlarmType.RtcWakeup, (long)(calendar.ToUniversalTime().Subtract(dtBasis).TotalMilliseconds), pendingIntent);
+
+
+        }
+
     }
 }
 
