5122694262824a715fd536a57e320ae2040fcf53
Hamzah Mansour <hamzahbmansour@gmail.com>
2018-07-09 00:25:42 -0700

    navigation and fragments added

---
+++ Mobile/DrWatch-mobile/DrWatch-android/Fragments/SettingsFragment.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+
+namespace DrWatch_android
+{
+    public class SettingsFragment : Android.Support.V4.App.Fragment
+    {
+        public override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your fragment here
+        }
+
+        public static SettingsFragment NewInstance()
+        {
+            var settingsFragment = new SettingsFragment { Arguments = new Bundle() };
+            return settingsFragment;
+        }
+
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            // Use this to return your custom view for this Fragment
+            var ignored = base.OnCreateView(inflater, container, savedInstanceState);
+            return inflater.Inflate(Resource.Layout.settings, container, false);
+        }
+    }
+}
