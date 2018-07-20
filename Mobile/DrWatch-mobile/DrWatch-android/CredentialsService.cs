f851833250e2e636f0fcbd90bc7a0472d4f2f004
Brendan <brendancb@users.noreply.github.com>
2018-07-19 17:31:39 -0700

    Added temporary non-default password.

--- Mobile/DrWatch-mobile/DrWatch-android/CredentialsService.cs
+++ Mobile/DrWatch-mobile/DrWatch-android/CredentialsService.cs
@@ -10,27 +10,27 @@ namespace DrWatch_android
         {
             if(account != null)
             {
-                AccountStore.Create().Save(account, "DrWatch_android");
+                AccountStore.Create("todopass").Save(account, "DrWatch_android");
             } 
         }
 
         public void DeleteCredentials()
         {
-            var account = AccountStore.Create().FindAccountsForService("DrWatch_android").FirstOrDefault();
+            var account = AccountStore.Create("todopass").FindAccountsForService("DrWatch_android").FirstOrDefault();
             if (account != null)
             {
-                AccountStore.Create().Delete(account, "DrWatch_android");
+                AccountStore.Create("todopass").Delete(account, "DrWatch_android");
             }
         }
 
         public bool DoCredentialsExist()
         {
-            return AccountStore.Create().FindAccountsForService("DrWatch_android").Any() ? true : false;
+            return AccountStore.Create("todopass").FindAccountsForService("DrWatch_android").Any() ? true : false;
         }
 
         public Account LoadStoredCredentials()
         {
-            var account = AccountStore.Create().FindAccountsForService("DrWatch_android").FirstOrDefault();
+            var account = AccountStore.Create("todopass").FindAccountsForService("DrWatch_android").FirstOrDefault();
             return account;
         }
     }
